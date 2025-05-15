c64910778cc91fabdf9c416564906100ad066c21
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-05-15 13:46:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // complete the given function
 
 function palindrome(str){
-
+	let str1=str;
+	str1.reverse();
+	if(str1==str){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 module.exports = palindrome
